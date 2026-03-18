5c949b2a68efa482eb206f9b24949cd8474f3125
unknown <sanikakamble854@gmail.com>
2026-03-18 20:30:09 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files
+app.use(express.static("public"));
+
+// Socket connection
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    // Receive message
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg); // Send to all users
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
